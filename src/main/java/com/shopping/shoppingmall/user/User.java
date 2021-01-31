15a04604f0887c987d3594d724daa43f465495ef
kl529 <kl529@naver.com>
2021-01-31 20:04:55 +0900

    jpa 연동 및 기본insert값 추가 및 controller, repository 추가

--- src/main/java/com/shopping/shoppingmall/user/User.java
+++ src/main/java/com/shopping/shoppingmall/user/User.java
@@ -6,17 +6,25 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.*;
 //bd3b0212-7a5a-4c0f-be45-3ac4bd106d83
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 //@JsonFilter("UserInfo") //보여주고 싶은 정보만 패키징해서 사용①
 //@JsonIgnoreProperties(value={"phone_number","address","email"}) //일반 유저가 검색하면 이름이랑 id만 보여지게
 
 @ApiModel(description = "사용자-상세 정보를 위한 도메인 객체")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message="Name은 2글자 이상 입력해 주세요")
---
+++ src/main/java/com/shopping/shoppingmall/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.shopping.shoppingmall.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
