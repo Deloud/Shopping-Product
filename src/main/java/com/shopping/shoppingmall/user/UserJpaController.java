fab6be8fd1ebcda8da45ea1bdb6a75974bc3a789
kl529 <kl529@naver.com>
2021-01-31 20:09:22 +0900

    jpa에서 hata 추가하고 findone 구현완료

--- src/main/java/com/shopping/shoppingmall/user/UserJpaController.java
+++ src/main/java/com/shopping/shoppingmall/user/UserJpaController.java
@@ -1,11 +1,17 @@
 package com.shopping.shoppingmall.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/jpa")
@@ -17,5 +23,20 @@ public class UserJpaController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public Resource<User> retrieveUser(int id){
+        Optional<User> user = userRepository.findById(id);
+
+        if (!user.isPresent()){
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        Resource<User> resource = new Resource<>(user.get());
+        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        resource.add(linkTo.withRel("all-users"));
+
+        return resource;
     }
 }
