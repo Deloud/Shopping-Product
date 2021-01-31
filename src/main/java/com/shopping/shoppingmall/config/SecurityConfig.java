a2a7b1c710400d1504168edb272930654177501d
kl529 <kl529@naver.com>
2021-01-31 19:49:59 +0900

    h2 데베 연결 기초, security 삭제

--- src/main/java/com/shopping/shoppingmall/config/SecurityConfig.java
+++ src/main/java/com/shopping/shoppingmall/config/SecurityConfig.java
@@ -1,9 +1,9 @@
 package com.shopping.shoppingmall.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 //@Configuration
 //public class SecurityConfig extends WebSecurityConfigurerAdapter {
