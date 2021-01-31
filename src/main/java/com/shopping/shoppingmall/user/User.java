70861179950ff3fd7b03ad13304499dc20ac548c
kl529 <kl529@naver.com>
2021-01-31 17:18:45 +0900

    swagger 완료

--- src/main/java/com/shopping/shoppingmall/user/User.java
+++ src/main/java/com/shopping/shoppingmall/user/User.java
@@ -2,6 +2,8 @@ package com.shopping.shoppingmall.user;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -11,12 +13,21 @@ import javax.validation.constraints.*;
 @AllArgsConstructor
 //@JsonFilter("UserInfo") //보여주고 싶은 정보만 패키징해서 사용①
 //@JsonIgnoreProperties(value={"phone_number","address","email"}) //일반 유저가 검색하면 이름이랑 id만 보여지게
+
+@ApiModel(description = "사용자-상세 정보를 위한 도메인 객체")
 public class User {
     private Integer id;
 
     @Size(min=2, message="Name은 2글자 이상 입력해 주세요")
+    @ApiModelProperty(notes = "사용자 이름을 입력해주세요") //SWAGGER
     private String name;
+    
+    @ApiModelProperty(notes = "사용자 휴대폰번호를 입력해주세요")
     private String phone_number;
+    
+    @ApiModelProperty(notes = "사용자 주소를 입력해주세요")
     private String address;
+    
+    @ApiModelProperty(notes = "사용자 이메일을 입력해주세요")
     private String email;
 }
