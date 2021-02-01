acbdb9ee012b389496a271eadaa6fdc19fdb6c92
karice <kl529@naver.com>
2021-02-01 20:49:42 +0900

    default setting done. product service add

---
+++ src/main/java/com/shopping/shoppingmall/product/Product.java
@@ -0,0 +1,4 @@
+package com.shopping.shoppingmall.product;
+
+public class Product {
+}
--- src/main/java/com/shopping/shoppingmall/user/UserJpaController.java
+++ src/main/java/com/shopping/shoppingmall/user/UserJpaController.java
@@ -22,6 +22,9 @@ public class UserJpaController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/users")
     public List<User> retrieveAllUsers(){
         return userRepository.findAll();
@@ -55,6 +58,37 @@ public class UserJpaController {
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
                 .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    //post
+    @GetMapping("/users/{id}/posts")
+    public List<Post> retrieveAllPostsByUser(@PathVariable int id){
+        Optional<User> user = userRepository.findById(id);
+
+        if (!user.isPresent()){
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return user.get().getPosts();
+    }
+    //post
+    @PostMapping("/users/{id}/posts")
+    public ResponseEntity<Post> createPost(@PathVariable int id, @RequestBody Post post){
+
+        Optional<User> user = userRepository.findById(id);
+
+        if (!user.isPresent()){
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+        post.setUser(user.get());
+        Post savedPost = postRepository.save(post);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedPost.getId())
                 .toUri();
 
         return ResponseEntity.created(location).build();
