8d2293a80654223c382aabf5763f2d1c01cc49fa
karice <kl529@naver.com>
2021-02-22 15:33:59 +0900

    유레카, 쥴 제거 (테스트 완료)

--- src/main/java/com/shopping/shoppingmallproduct/ShoppingMallProductApplication.java
+++ src/main/java/com/shopping/shoppingmallproduct/ShoppingMallProductApplication.java
@@ -2,10 +2,10 @@ package com.shopping.shoppingmallproduct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class ShoppingMallProductApplication {
 
     public static void main(String[] args) {
