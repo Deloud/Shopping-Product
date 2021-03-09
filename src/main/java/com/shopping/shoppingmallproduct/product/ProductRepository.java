7f58f664f47ecdc9a520cdd63452144bd7cf7289
JeongEunJi <wjdwl270@naver.com>
2021-03-09 21:41:48 +0900

    [Delete] master code

--- src/main/java/com/shopping/shoppingmallproduct/product/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shopping.shoppingmallproduct.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
