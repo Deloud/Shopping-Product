2e9e82b0e49747403f42abe91fa77f99d35a321d
BenKwon <kwon5891@gmail.com>
2021-04-28 15:44:45 +0900

    [Update] contract test 실패 테스트 성공후 컨트롤러 정상화

--- Shopping-Product/src/main/java/com/shopping/product/product/product/ProductController.java
+++ Shopping-Product/src/main/java/com/shopping/product/product/product/ProductController.java
@@ -38,19 +38,19 @@ public class ProductController {
 ////        });
 //        return "test";
 //    }
-    @GetMapping("/{id}")
+    @GetMapping()
     public ResponseEntity<Integer> home() throws ParseException {
 
         if (count != 3) {
             count++;
-            return ResponseEntity.status(200).body(1);
+            return ResponseEntity.status(500).body(1);
 
         }
-        return ResponseEntity.status(200).body(-1);
+        return ResponseEntity.status(500).body(-1);
 
     }
 
-    @GetMapping()
+    @GetMapping("/{id}")
     public JSONObject retrieveProduct(@PathVariable int id) throws ParseException {
         String output = "{" + "name:" + resturant.getNametoid(id) + ", price:" + resturant.getPricetoid(id) + "}";
 
